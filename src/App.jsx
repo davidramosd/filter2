908a1f6f4276ad37de85b88dc85fa181cbcd0847
SantiagoRt1 <165504777+SantiagoRt1@users.noreply.github.com>
2024-04-01 19:38:59 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -119,65 +119,7 @@ function App() {
           <p>El repositorio consta de 56 registros de señales PPG, cada uno con una duración de 2 minutos y una frecuencia de muestreo de 100 Hz. Cada registro representa a un individuo que participó en el estudio. Es importante destacar que ninguno de los sujetos incluidos en esta base de datos se encontraba hospitalizado en el momento de la toma de datos. Todas las mediciones se realizaron en un entorno controlado, específicamente en un laboratorio.</p>
           <p>Esta información incluye si el individuo está diagnosticado con hipertensión, si está recibiendo tratamiento y cómo se clasifica según los criterios establecidos por el Comité de Expertos en el Tratamiento de la Hipertensión Arterial (JNC, por sus siglas en inglés). Las clasificaciones incluyen Normotenso (N), Prehipertenso (E) e Hipertenso (H), según los valores de presión arterial sistólica y diastólica.</p>
           <p>Esta base de datos representa una herramienta valiosa para investigadores y profesionales de la salud que buscan desarrollar métodos precisos y no invasivos para la estimación de la presión arterial. Al permitir el acceso a datos clínicamente relevantes y señales fisiológicas detalladas, se espera que esta base de datos contribuya significativamente al avance en el campo de la monitorización y el diagnóstico de la presión arterial, así como en la mejora de las estrategias de tratamiento para pacientes con hipertensión arterial. Aqui podrás filtrar y descargar el registro que necesites para poder visualizar la señal a travez de la plataforma de MATLAB</p>
-		<p> A continuación podras observar los 56 registros 
-
-PPG_subj_01	44	M	y	n	124	83	76	E	H
-PPG_subj_02	58	M	y	y	115	75	84	N	N
-PPG_subj_03	57	F	y	y	121	72	56	E	E
-PPG_subj_04	48	F	y	y	125	75	73	E	E
-PPG_subj_05	46	F	y	y	97	64	86	N	N
-PPG_subj_06	65	F	y	y	119	57	85	N	N
-PPG_subj_07	60	F	y	y	161	94	68	H	H
-PPG_subj_08	47	M	y	n	148	96	58	H	H
-PPG_subj_09	46	F	y	y	122	74	62	E	E
-PPG_subj_10	58	F	y	y	114	71	75	N	N
-PPG_subj_11	57	M	y	y	112	72	75	N	N
-PPG_subj_12	65	M	y	n	111	70	75	N	N
-PPG_subj_13	45	F	n	n	144	87	65	H	H
-PPG_subj_14	45	F	n	n	130	90	88	H	H
-PPG_subj_15	45	F	n	n	103	65	97	N	N
-PPG_subj_16	48	F	y	n	152	109	100	H	H
-PPG_subj_17	65	F	n	n	133	81	77	E	H
-PPG_subj_18	45	F	n	n	135	90	84	H	H
-PPG_subj_19	57	F	n	n	120	68	58	N	E
-PPG_subj_20	53	F	y	n	154	93	62	H	H
-PPG_subj_21	55	F	n	n	125	84	62	E	H
-PPG_subj_22	45	F	n	n	106	60	53	N	N
-PPG_subj_23	45	M	y	y	127	82	71	E	H
-PPG_subj_24	57	F	y	y	110	60	78	N	N
-PPG_subj_25	45	F	n	n	115	60	72	N	N
-PPG_subj_26	45	F	y	y	137	90	69	H	H
-PPG_subj_27	56	F	n	n	135	97	69	H	H
-PPG_subj_28	45	F	n	n	120	78	70	N	E
-PPG_subj_29	50	F	y	y	135	83	76	E	H
-PPG_subj_30	45	M	n	n	111	78	72	N	N
-PPG_subj_31	46	M	y	y	129	82	72	E	H
-PPG_subj_32	50	M	n	n	129	79	68	E	E
-PPG_subj_33	64	M	n	n	125	75	78	E	E
-PPG_subj_34	48	M	n	n	130	87	80	E	H
-PPG_subj_35	62	M	y	y	153	88	84	H	H
-PPG_subj_36	55	M	n	n	122	72	81	E	E
-PPG_subj_37	57	M	n	n	127	80	83	E	H
-PPG_subj_38	63	M	n	n	121	87	68	E	H
-PPG_subj_39	48	M	n	n	132	83	90	E	H
-PPG_subj_40	65	F	n	n	116	79	106	N	N
-PPG_subj_41	60	M	y	y	140	91	54	H	H
-PPG_subj_42	60	F	y	y	144	86	68	H	H
-PPG_subj_43	45	F	n	n	154	96	65	H	H
-PPG_subj_44	53	F	y	n	164	93	62	H	H
-PPG_subj_45	45	F	y	y	142	90	69	H	H
-PPG_subj_46	56	F	n	n	145	90	69	H	H
-PPG_subj_47	46	F	y	y	118	75	86	N	N
-PPG_subj_48	45	F	n	n	107	68	97	N	N
-PPG_subj_49	45	F	n	n	118	62	72	N	N
-PPG_subj_50	55	M	n	n	119	74	81	N	N
-PPG_subj_51	57	M	n	n	114	75	83	N	N
-PPG_subj_52	58	F	y	y	128	72	75	E	E
-PPG_subj_53	65	M	y	n	121	75	75	E	E
-PPG_subj_54	58	M	y	n	134	85	76	E	H
-PPG_subj_55	45	M	y	y	128	83	71	E	H
-PPG_subj_56	46	F	y	y	128	89	78	E	H
-    </p>
+		<p> A continuación podras filtrar según las caracteristicas que necesites cualquiera de los 56 registros</p>
         </div>
       {
         showForm
